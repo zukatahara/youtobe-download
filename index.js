7e7777e74b0262012e4ec44e1ff98022ecb94e73
unknown <quyenpro7a@gmail.com>
2023-01-27 18:07:34 +0700

    abc

--- index.js
+++ index.js
@@ -8,17 +8,20 @@ const path = require("path");
 const fs = require("fs");
 const app = express();
 app.get("/", (req, res) => {
+  var downloadFolder = path.join(process.env.USERPROFILE, "/Downloads/");
+
   const url = req.query.url
     ? req.query.url
-    :"https://www.youtube.com/embed/wz-ZkPEGyeo?start=10&end=15";
-    // : "https://www.youtube.com/watch?v=wz-ZkPEGyeo?start=10&end=15";
+    : "https://www.youtube.com/embed/wz-ZkPEGyeo?start=10&end=15";
+  // : "https://www.youtube.com/watch?v=wz-ZkPEGyeo?start=10&end=15";
   // console.log('url: ', url);return
   // const output = path.resolve(__dirname, `${Date.now()}_video.mp4`);
 
   // const video = ytdl.downloadFromInfo(url,{filter:'audioandvideo'});
   // video.pipe(fs.createWriteStream(output));
-  ytdl(url, { filter: "audioonly",begin:"1m30s"}).pipe(
-    fs.createWriteStream(`${Date.now()}_video.mp3`)
+  ytdl(url, { filter: "audioonly" }).pipe(
+    fs.createWriteStream(`${downloadFolder}${Date.now()}_video.mp3`)
   );
 });
 app.listen(3000);
+
